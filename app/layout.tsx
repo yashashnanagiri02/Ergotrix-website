2531418e928eab5b93ecc486a2cb2701f5c7c08b
Yashash Nanagiri <yashashnanagiri02@gmail.com>
2026-07-30 14:08:26 +0530

    Updated Hero Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
-
+export const metadata = {
+  title: "ERGOTRIX Engineering Solutions Pvt Ltd",
+  description:
+    "Engineering Excellence. Innovation Delivered. Innovative engineering and product development solutions.",
+};
 export default function RootLayout({
   children,
 }: {
